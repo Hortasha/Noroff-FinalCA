c24bef55bc278e8897d6525d8a45c22cfae70e17
hortasha <eidemartin_303@hotmail.com>
2019-04-09 11:01:21 +0200

    Fix bugs

--- src/app/services/contact/contact.service.ts
+++ src/app/services/contact/contact.service.ts
@@ -19,12 +19,12 @@ export class ContactService {
       valid[4] = false;
     }
 
-    if((/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/).test(formValues.phone) === false) {
+    if((/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]{4,25}$/).test(formValues.phone) === false) {
       valid[2] = false;
       valid[4] = false;
     }
 
-    if((/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/igm).test(formValues.mail) === false) {
+    if((/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/).test(formValues.mail) === false) {
       valid[3] = false;
       valid[4] = false;
     }
